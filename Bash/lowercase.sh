a88f6776b9f6edebbf9b0c9b201f627dda261e4b
Todd Hogarth <HOGARTHT16@ecu.edu>
2017-06-22 12:35:19 -0400

    variable expansion hell

---
+++ Bash/lowercase.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# puzzle source: https://www.codeeval.com/open_challenges/20/
+# Given a string write a program to convert it into lowercase.
+
+file="$1"
+IFS_OLD=$IFS
+IFS="\n"
+while read -r line || [[ -n "$line" ]]
+do
+    declare -l lcase
+    for((i=0;i<${#line};i++))
+    do
+        lcase="${line:$i:1}"
+        printf '%s' "$lcase"
+    done
+    printf "\n"
+done < "$file"
+IFS=$IFS_OLD
